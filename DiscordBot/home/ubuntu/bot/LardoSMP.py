0a7d68b469aaea6a28b330618753800132403302
Ashton Gibson <59347006+homeless-field@users.noreply.github.com>
2024-07-12 20:56:04 -0700

    Update LardoSMP.py
    Added the ("secret") -roles command, to add reaction roles to the Discord server

--- DiscordBot/home/ubuntu/bot/LardoSMP.py
+++ DiscordBot/home/ubuntu/bot/LardoSMP.py
@@ -17,6 +17,10 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='-', intents=intents)
 
 @bot.command()
+async def roles(ctx):
+	await ctx.send("""Hark, stranger! Claim yon clan[s] (colloquially known as a "role") in this message!\n\n<:lardo:707341985186316298> ``@Regulars``: This group of yung maidens LOVES being pinged in the rear guard. Choose this role if you wish'ith to frequent these parts\n<:sand_tsunami:779440160232308737> ``@LardoSMP``: Listen here lad, that business with Dream is IN THE PAST\n<:dung_eater:962574208989265991> ``@Leagulars``: All worthy kingdoms have a latrine pit. This is ours. Prithee be careful\n<:triple_chocolate_crepe:877749423566897193> ``@DnD``: Nerrrd\n<:adodo:973061689102721104> ``@Warframe``: Feeling laborious? Why not take a stroll by our Orokin-inspired "open" "pasture" farms?\n<:tim_reaction:973064203571171378> ``@Fighting Games``: Yo yo yo, let's jus _**FUCKIN**_ kill each other""")
+
+@bot.command()
 async def startserver(ctx):
 	ec2Response = ec2.start_instances(InstanceIds=['INSTANCE ID',],)
 	await ctx.send("Host's a-kickin'! If that Minecraft server don't mosey on up in 5 minutes, you better wrangle up ``@ADMIN USERNAME``. \n\nWe're ridin' on the IP ``SERVER IP``! Yeehaw! 🤠🌵🔫")
